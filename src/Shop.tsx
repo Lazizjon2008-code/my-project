15af2728c8853d48954a7c10b16a40a3c089632d
Lazizjon2008-code <your-no-reply-laziizjon04@gmail.comusers.noreply.github.com>
2025-03-24 14:50:08 +0500

    ended shop page

---
+++ src/Shop.tsx
@@ -0,0 +1,268 @@
+import './Shop.css'
+
+function Shop() {
+    return (
+        <>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+    <header>
+      <div className="container">
+        <nav className='navbar'>
+          <div className='headers'>
+            <div className='logo'>
+              <img src="src/images/Logo.svg" alt="" />
+            </div>
+
+            <div className='connect_pages'>
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+
+            <div className='btns_of_header'>
+              <button><img src="src/images/blog.svg" alt="" /></button>
+              <button><img src="src/images/search.svg" alt="" /></button>
+              <button><img src="src/images/likes.svg" alt="" /></button>
+              <button><img src="src/images/shop.svg" alt="" /></button>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <section className='hero_section'>
+        <div className="container">
+            <div className='hero_title'>
+                <h2>Shop</h2>
+                <h5>Home <img src="src/images/vector.svg" alt="" /> Shop</h5>
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <div className="container">
+            <div className='purchase_smth'>
+                <div className='products'>
+                    <div className='about_Syltherine'>
+                        <img src="src/images/Syltherine.svg" alt="" />
+                        <h4>Syltherine</h4>
+                        <p>Stylish cafe chair</p>
+                        <div className='prices'>
+                        <h5>Rp 2.500.000</h5>
+                        <p>Rp 3.500.000</p>
+                        </div>
+                    </div>
+                    <div className='about_Leviosa'>
+                        <img src="src/images/Syltherine.svg" alt="" />
+                        <h4>Leviosa</h4>
+                        <p>Stylish cafe chair</p>
+                        <h5>Rp 2.500.000</h5>
+                    </div>
+                    <div className='about_Lolito'>
+                        <img src="src/images/lolito.svg" alt="" />
+                        <h4>Lolito</h4>
+                        <p>Luxury big sofa</p>
+                        <div className='prices'>
+                        <h5>Rp 7.000.000</h5>
+                        <p>Rp 14.000.000</p>
+                        </div>
+                    </div>
+                    <div className='about_Respira'>
+                        <img src="src/images/respira.svg" alt="" />
+                        <h4>Respira</h4>
+                        <p>Outdoor bar table and stool</p>
+                        <h5>Rp 500.000</h5>
+                    </div>
+                </div>
+                <div className='products'>
+                    <div className='about_Syltherine'>
+                    <img src="src/images/Syltherine.svg" alt="" />
+                    <h4>Syltherine</h4>
+                    <p>Stylish cafe chair</p>
+                    <div className='prices'>
+                    <h5>Rp 2.500.000</h5>
+                    <p>Rp 3.500.000</p>
+                    </div>
+                </div>
+                <div className='about_Leviosa'>
+                    <img src="src/images/Syltherine.svg" alt="" />
+                    <h4>Leviosa</h4>
+                    <p>Stylish cafe chair</p>
+                    <h5>Rp 2.500.000</h5>
+                </div>
+                <div className='about_Lolito'>
+                    <img src="src/images/lolito.svg" alt="" />
+                    <h4>Lolito</h4>
+                    <p>Luxury big sofa</p>
+                    <div className='prices'>
+                    <h5>Rp 7.000.000</h5>
+                    <p>Rp 14.000.000</p>
+                    </div>
+                </div>
+                <div className='about_Respira'>
+                    <img src="src/images/respira.svg" alt="" />
+                    <h4>Respira</h4>
+                    <p>Outdoor bar table and stool</p>
+                    <h5>Rp 500.000</h5>
+                </div>
+            </div>
+            <div className='products'>
+              <div className='about_Syltherine'>
+                <img src="src/images/Syltherine.svg" alt="" />
+                <h4>Syltherine</h4>
+                <p>Stylish cafe chair</p>
+                <div className='prices'>
+                  <h5>Rp 2.500.000</h5>
+                  <p>Rp 3.500.000</p>
+                </div>
+              </div>
+              <div className='about_Leviosa'>
+                <img src="src/images/Syltherine.svg" alt="" />
+                <h4>Leviosa</h4>
+                <p>Stylish cafe chair</p>
+                <h5>Rp 2.500.000</h5>
+              </div>
+              <div className='about_Lolito'>
+                <img src="src/images/lolito.svg" alt="" />
+                <h4>Lolito</h4>
+                <p>Luxury big sofa</p>
+                <div className='prices'>
+                  <h5>Rp 7.000.000</h5>
+                  <p>Rp 14.000.000</p>
+                </div>
+              </div>
+              <div className='about_Respira'>
+                <img src="src/images/respira.svg" alt="" />
+                <h4>Respira</h4>
+                <p>Outdoor bar table and stool</p>
+                <h5>Rp 500.000</h5>
+              </div>
+            </div>
+            <div className='products'>
+              <div className='about_Syltherine'>
+                <img src="src/images/Syltherine.svg" alt="" />
+                <h4>Syltherine</h4>
+                <p>Stylish cafe chair</p>
+                <div className='prices'>
+                  <h5>Rp 2.500.000</h5>
+                  <p>Rp 3.500.000</p>
+                </div>
+              </div>
+              <div className='about_Leviosa'>
+                <img src="src/images/Syltherine.svg" alt="" />
+                <h4>Leviosa</h4>
+                <p>Stylish cafe chair</p>
+                <h5>Rp 2.500.000</h5>
+              </div>
+              <div className='about_Lolito'>
+                <img src="src/images/lolito.svg" alt="" />
+                <h4>Lolito</h4>
+                <p>Luxury big sofa</p>
+                <div className='prices'>
+                  <h5>Rp 7.000.000</h5>
+                  <p>Rp 14.000.000</p>
+                </div>
+              </div>
+              <div className='about_Respira'>
+                <img src="src/images/respira.svg" alt="" />
+                <h4>Respira</h4>
+                <p>Outdoor bar table and stool</p>
+                <h5>Rp 500.000</h5>
+              </div>
+            </div>
+            </div>
+            <div className='btns_of_shop'>
+                <button className='page_btn'>1</button>
+                <button className='page_btn'>2</button>
+                <button className='page_btn'>3</button>
+                <button className='next_btn'>Next</button>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div className="container">
+            <div className='about_shop'>
+                <div className='quality'>
+                    <img src="src/images/trophy.svg" alt="" />
+                    <div>
+                        <h4>High Quality</h4>
+                        <p>crafted from top materials</p>
+                    </div>
+                </div>
+                <div className='quality'>
+                    <img src="src/images/true.svg" alt="" />
+                    <div>
+                        <h4>Warranty Protection</h4>
+                        <p>Over 2 years</p>
+                    </div>
+                </div>
+                <div className='quality'>
+                    <img src="src/images/shipping.svg" alt="" />
+                    <div>
+                        <h4>Free Shipping</h4>
+                        <p>Order over 150 $</p>
+                    </div>
+                </div>
+                <div className='quality'>
+                    <img src="src/images/operator.svg" alt="" />
+                    <div>
+                        <h4>24 / 7 Support</h4>
+                        <p>Dedicated support</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer className="footer">
+      <div className="footer-container">    
+        <div className="footer-section company-info">
+          <h2 className="logo">Funiro.</h2>
+          <address>
+            400 University Drive Suite 200 Coral <br /> Gables, <br />
+            FL 33134 USA
+          </address>
+        </div>
+
+        
+        <div className="footer-section links-help many-links">
+          <div className="footer-links">
+            <h4>Links</h4>
+            <a href="#">Home</a>
+            <a href="#">Shop</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </div>
+
+          <div className="footer-help">
+            <h4>Help</h4>
+            <a href="#">Payment Options</a>
+            <a href="#">Returns</a>
+            <a href="#">Privacy Policies</a>
+          </div>
+        </div>
+
+        
+        <div className="footer-section newsletter">
+          <h4>Newsletter</h4>
+          <form className="newsletter-form">
+            <input type="email" placeholder="Enter Your Email Address" className="email-input" required/>
+            <button type="submit" className="subscribe-btn">SUBSCRIBE</button>
+          </form>
+        </div>
+
+      </div>
+
+      
+      <div className="footer-bottom">
+        <p>2023 funiro. All rights reserved</p>
+      </div>
+    </footer>
+    </>
+    )
+}
+
+export default Shop
